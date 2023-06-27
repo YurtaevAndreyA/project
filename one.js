47fdcf176a21ba0daa1e60b1d6057b7aa61f4e8a
YurtaevAndreyA <YurtaevAndreyA@gmail.com>
2023-06-27 19:11:59 +0300

    3 commit

--- one.js
+++ one.js
@@ -2,3 +2,10 @@
 //create '.gitignore' and add to it /.idea
 // git add - file(s) and '.gitignore - green'
 // first commit
+
+
+// А тут напишем Function Declaration, такую же по смыслу, как и в файле two.js
+
+function f(n) {
+		return n ** 2;
+}
---
+++ two.js
@@ -0,0 +1,8 @@
+
+let f1 = function(n) {
+		return n ** 2;
+}
+
+// Тоже самое, что и
+
+let f2 = n => n ** 2;
